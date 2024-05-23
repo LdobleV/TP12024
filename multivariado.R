b774c05881d202665cb7b3e2b3d58700819ba0c3
EliseoVallejos <eliseovallejos01@gmail.com>
2024-05-23 17:47:29 -0300

    Actualizado archivo de graficas bivariadas

--- multivariado.R
+++ multivariado.R
@@ -20,67 +20,39 @@ ggplot(datos, aes(x=`¿Cuál es el costo actual del mismo?`, y = hacinamiento)) 
 
 ###desague y plagas###
 
+calcular <- function(ColTotal, ColCond, CondColTotal, CondColCond) {
+  viviendas_que_cumplen_ambas <- datos %>%
+    filter(ColTotal == CondColTotal &  ColCond == CondColCond) %>%
+    nrow()
+  
+  total <- datos %>%
+    filter(ColCond == CondColCond) %>%
+    nrow()
+  
+  return((viviendas_que_cumplen_ambas / total) * 100)
+}
 
-# Filtrar las viviendas con pozo negro y que presentan plagas
-viviendas_pozo_negro_plagas <- datos %>%
-  filter(`¿Qué tipo de desagüe posee la vivienda?` == "A pozo negro/ciego" & `¿Hay plagas (cucarachas, mosquitos, ratas, etc) en su vivienda y en los alrededores de la misma?` == "Sí") %>%
-  nrow()
+porcentaje_plagas_pozo_negro <- calcular(`¿Qué tipo de desagüe posee la vivienda?`,
+               `¿Hay plagas (cucarachas, mosquitos, ratas, etc) en su vivienda y en los alrededores de la misma?`,
+               "A pozo negro/ciego",
+               "Sí")
 
+porcentaje_plagas_cloaca <- calcular(`¿Qué tipo de desagüe posee la vivienda?`,
+                                         `¿Hay plagas (cucarachas, mosquitos, ratas, etc) en su vivienda y en los alrededores de la misma?`,
+                                         "Desagüe a red cloacal pública",
+                                         "Sí")
 
-# Contar el número total de viviendas con pozo negro
-total_viviendas_pozo_negro <- datos %>%
-  filter(`¿Qué tipo de desagüe posee la vivienda?` == "A pozo negro/ciego") %>%
-  nrow()
-
-# Calcular el porcentaje
-porcentaje_plagas_pozo_negro <- (viviendas_pozo_negro_plagas / total_viviendas_pozo_negro) * 100
-####
-
-# Filtrar las viviendas con pozo negro y que presentan plagas
-viviendas_cloaca_plagas <- datos %>%
-  filter(`¿Qué tipo de desagüe posee la vivienda?` == "Desagüe a red cloacal pública" & `¿Hay plagas (cucarachas, mosquitos, ratas, etc) en su vivienda y en los alrededores de la misma?` == "Sí") %>%
-  nrow()
-
-
-# Contar el número total de viviendas con pozo negro
-total_viviendas_cloaca <- datos %>%
-  filter(`¿Qué tipo de desagüe posee la vivienda?` == "Desagüe a red cloacal pública") %>%
-  nrow()
-
-# Calcular el porcentaje
-porcentaje_plagas_cloaca <- (viviendas_cloaca_plagas / total_viviendas_cloaca) * 100
-
-####
-
-# Filtrar las viviendas con pozo negro y que presentan plagas
-viviendas_cloaca_inf_plagas <- datos %>%
-  filter(`¿Qué tipo de desagüe posee la vivienda?` == "Desagüe a red cloacal informal/vecinal" & `¿Hay plagas (cucarachas, mosquitos, ratas, etc) en su vivienda y en los alrededores de la misma?` == "Sí") %>%
-  nrow()
+porcentaje_plagas_cloaca_inf <- calcular(`¿Qué tipo de desagüe posee la vivienda?`,
+                                     `¿Hay plagas (cucarachas, mosquitos, ratas, etc) en su vivienda y en los alrededores de la misma?`,
+                                     "Desagüe a red cloacal informal/vecinal",
+                                     "Sí")
 
 
-# Contar el número total de viviendas con pozo negro
-total_viviendas_cloaca_inf <- datos %>%
-  filter(`¿Qué tipo de desagüe posee la vivienda?` == "Desagüe a red cloacal informal/vecinal") %>%
-  nrow()
+porcentaje_plagas_camara_sep <- calcular(`¿Qué tipo de desagüe posee la vivienda?`,
+                                         `¿Hay plagas (cucarachas, mosquitos, ratas, etc) en su vivienda y en los alrededores de la misma?`,
+                                         "A cámara séptica",
+                                         "Sí")
 
-# Calcular el porcentaje
-porcentaje_plagas_cloaca_inf <- (viviendas_cloaca_inf_plagas / total_viviendas_cloaca_inf) * 100
-
-####
-
-# Filtrar las viviendas con pozo negro y que presentan plagas
-viviendas_camara_sep_plagas <- datos %>%
-  filter(`¿Qué tipo de desagüe posee la vivienda?` == "A cámara séptica" & `¿Hay plagas (cucarachas, mosquitos, ratas, etc) en su vivienda y en los alrededores de la misma?` == "Sí") %>%
-  nrow()
-
-
-# Contar el número total de viviendas con pozo negro
-total_viviendas_camara_sep <- datos %>%
-  filter(`¿Qué tipo de desagüe posee la vivienda?` == "A cámara séptica") %>%
-  nrow()
-
-# Calcular el porcentaje
-porcentaje_plagas_camara_sep <- (viviendas_camara_sep_plagas / total_viviendas_camara_sep) * 100
 
 # Crear un data frame con los resultados
 resultados <- data.frame(
@@ -99,8 +71,93 @@ ggplot(resultados, aes(x = Tipo, y = Porcentaje, fill = Tipo)) +
   geom_text(aes(label = paste0(round(Porcentaje, 1), "%")), vjust = -0.5) + 
   ylim(0, 100)
 
-###dg
+###plagas distancia a basurales###
+
+porcentaje_plagas_sinbasurales <- calcular(`¿Hay basurales cerca de su vivienda?`,
+                                         `¿Hay plagas (cucarachas, mosquitos, ratas, etc) en su vivienda y en los alrededores de la misma?`,
+                                         "No",
+                                         "Sí")
+
+porcentaje_plagas_500m <- calcular(`¿Hay basurales cerca de su vivienda?`,
+                                   `¿Hay plagas (cucarachas, mosquitos, ratas, etc) en su vivienda y en los alrededores de la misma?`,
+                                    "Sí, a menos de 500 metros",
+                                    "Sí")
 
 
+porcentaje_plagas_2km <- calcular(`¿Hay basurales cerca de su vivienda?`,
+                                   `¿Hay plagas (cucarachas, mosquitos, ratas, etc) en su vivienda y en los alrededores de la misma?`,
+                                   "Sí, a más de 500 metros y menos de 2 kilómetros",
+                                   "Sí")
 
+# Crear un data frame con los resultados
+resultados <- data.frame(
+  Tipo = c("No", "Sí, a menos de 500 metros", "Sí, a más de 500 metros y menos de 2 kilómetros"),
+  Porcentaje = c(porcentaje_plagas_sinbasurales, porcentaje_plagas_500m, porcentaje_plagas_2km)
+)
+
+
+# Crear la gráfica de barras del porcentaje de presencia de cada plaga
+ggplot(resultados, aes(x = Tipo, y = Porcentaje, fill = Tipo)) +
+  geom_bar(stat = "identity",color = "black", fill = "blue") +
+  theme_minimal() +
+  labs(title = "Porcentaje de plagas, segun la distancia a basurales",
+       x = "distancia a basurales",
+       y = "Porcentaje de plagas") +
+  geom_text(aes(label = paste0(round(Porcentaje, 1), "%")), vjust = -0.5) + 
+  ylim(0, 100)
+
+
+###plagas segun timepo de recoleccion###
+
+porcentaje_plagas_1semana <- calcular(`¿Con qué frecuencia el Municipio recolecta los residuos en sus inmediaciones?`,
+                                           `¿Hay plagas (cucarachas, mosquitos, ratas, etc) en su vivienda y en los alrededores de la misma?`,
+                                           "Una vez a la semana",
+                                           "Sí")
+
+porcentaje_plagas_2_4semana <- calcular(`¿Con qué frecuencia el Municipio recolecta los residuos en sus inmediaciones?`,
+                                      `¿Hay plagas (cucarachas, mosquitos, ratas, etc) en su vivienda y en los alrededores de la misma?`,
+                                      "Entre 2 y 4 veces a la semana",
+                                      "Sí")
+
+porcentaje_plagas_5semana <- calcular(`¿Con qué frecuencia el Municipio recolecta los residuos en sus inmediaciones?`,
+                                        `¿Hay plagas (cucarachas, mosquitos, ratas, etc) en su vivienda y en los alrededores de la misma?`,
+                                        "Al menos 5 veces a la semana",
+                                        "Sí")
+
+porcentaje_plagas_sinrecoleccion <- calcular(`¿Con qué frecuencia el Municipio recolecta los residuos en sus inmediaciones?`,
+                                      `¿Hay plagas (cucarachas, mosquitos, ratas, etc) en su vivienda y en los alrededores de la misma?`,
+                                      "No hay servicio de recolección municipal",
+                                      "Sí")
+
+# Crear un data frame con los resultados
+resultados <- data.frame(
+  Tipo = c("No hay servicio de recolección municipal", "Una vez a la semana", "Entre 2 y 4 veces a la semana", "Al menos 5 veces a la semana"),
+  Porcentaje = c(porcentaje_plagas_sinrecoleccion, porcentaje_plagas_1semana, porcentaje_plagas_5semana, porcentaje_plagas_sinrecoleccion)
+)
+
+
+# Crear la gráfica de barras del porcentaje de presencia de cada plaga
+ggplot(resultados, aes(x = Tipo, y = Porcentaje, fill = Tipo)) +
+  geom_bar(stat = "identity",color = "black", fill = "blue") +
+  theme_minimal() +
+  labs(title = "Porcentaje de plagas, segun frecuencia de recoleccion de basura",
+       x = "frecuencia de recoleccion",
+       y = "Porcentaje de plagas") +
+  geom_text(aes(label = paste0(round(Porcentaje, 1), "%")), vjust = -0.5) + 
+  ylim(0, 100)
+
+###
+
+# Filtrar los datos para eliminar las filas con NA en precio_de_alquiler
+datos1 <- datos %>%
+  filter(!is.na(`¿Cuál es el costo actual del mismo?`))
+
+# Crear los boxplots usando ggplot2
+ggplot(datos1, aes(x = `¿Hay plagas (cucarachas, mosquitos, ratas, etc) en su vivienda y en los alrededores de la misma?`, y = `¿Cuál es el costo actual del mismo?`, fill = `¿Hay plagas (cucarachas, mosquitos, ratas, etc) en su vivienda y en los alrededores de la misma?`)) +
+  geom_boxplot() +
+  ggtitle("Precio de Alquiler vs Presencia de Plagas") +
+  xlab("Presencia de Plagas") +
+  ylab("Precio de Alquiler") +
+  scale_fill_manual(values = c("Sí" = "red", "No" = "green")) +
+  theme_minimal()
 
